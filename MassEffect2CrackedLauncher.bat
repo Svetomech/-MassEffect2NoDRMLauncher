a46ccebad56ef4f0a996101d6a563db3383b8279
ANDREY BELYAEV <svetomech@gmail.com>
2017-04-26 02:16:09 +0300

    Does not require admin rights at first run now
    Changed DLC activation method to a more reliable one

--- MassEffect2CrackedLauncher.bat
+++ MassEffect2CrackedLauncher.bat
@@ -10,7 +10,7 @@ set "errorlevel=0"
 :: Application variables
 set "CompanyName=Svetomech"
 set "ProductName=MassEffect2CrackedLauncher"
-set "ProductVersion=1.1.0.0"
+set "ProductVersion=1.2.0.0"
 
 :: Global variables
 set "DesiredAppDirectory=%LocalAppData%\%CompanyName%\%ProductName%"
@@ -48,29 +48,14 @@ if not "%errorlevel%"=="0" (
     goto Exit
 )
 
-:: Check admin rights
-call :IsElevatedCMD
-if "%errorlevel%"=="0" (
-    set "isElevated=true"
-)
-
 :: Check connection
 call :HasInternetAccess
 if "%errorlevel%"=="0" (
     set "isOnline=true"
 )
 
-:: Prepare crack
-echo $client = New-Object System.Net.WebClient> "%cd%\%ProductName%_helper.ps1"
-echo $client.DownloadFile("https://bitbucket.org/Svetomech/masseffect2crackedlauncher/downloads/MassEffect2.exe", "%cd%\Binaries\MassEffect2.exe.cracked")>> "%cd%\%ProductName%_helper.ps1"
-
 :: Apply crack, unlock DLC
 if not "%CrackApplied%"=="true" (
-    if not defined isElevated (
-        call :WriteLog "First launch requires Administrator privileges!"
-        goto Exit
-    )
-
     if not defined isOnline (
         call :WriteLog "First launch requires Internet connection!"
         goto Exit
@@ -78,13 +63,15 @@ if not "%CrackApplied%"=="true" (
 
     call :WriteLog "Cracking the game..."
     rename "%cd%\Binaries\MassEffect2.exe" "MassEffect2.exe.original" >nul 2>&1
-    powershell -nologo -noprofile -executionpolicy bypass -file "%cd%\%ProductName%_helper.ps1"
+    call :DownloadFile "https://bitbucket.org/Svetomech/masseffect2crackedlauncher/downloads/MassEffect2.exe" "%cd%\Binaries\MassEffect2.exe.cracked"
     rename "%cd%\Binaries\MassEffect2.exe.cracked" "MassEffect2.exe" >nul 2>&1
 
     call :WriteLog "Unlocking DLC..."
-    echo 127.0.0.1 eame2blaze01.ea.com>> "%windir%\System32\drivers\etc\hosts"
+    rename "%cd%\Binaries\binkw32.dll" "binkw23.dll" >nul 2>&1
+    call :DownloadFile "https://bitbucket.org/Svetomech/masseffect2crackedlauncher/downloads/binkw32.dll" "%cd%\Binaries\binkw32.dll.cracked"
+    rename "%cd%\Binaries\binkw32.dll.cracked" "binkw32.dll" >nul 2>&1
 
-    call :SaveSetting "CrackApplied" true
+    call :SaveSetting "CrackApplied" "true"
 )
 
 call :WriteLog "Launching the game..."
@@ -97,19 +84,19 @@ goto Exit
 :LoadSetting
 for /f "tokens=1  delims=[]" %%n in ('find /i /n "%~1" ^<"%MainConfig%"') do set /a "$n=%%n+1"
 for /f "tokens=1* delims=[]" %%a in ('find /n /v "" ^<"%MainConfig%"^|findstr /b "\[%$n%\]"') do set "%~2=%%b"
-exit /b 0
+exit /b
 
 :: key, value
 :SaveSetting
 echo %~1    %date% %time%>> "%MainConfig%"
 echo %~2>> "%MainConfig%"
 echo.>> "%MainConfig%"
-exit /b 0
+exit /b
 
 :: message
 :WriteLog
 echo %me%: %~1
-exit /b 0
+exit /b
 
 ::
 :IsDirectoryValid
@@ -119,11 +106,11 @@ if not exist "%cd%\Binaries\MassEffect2.exe" set "errorlevel=1"
 if not exist "%cd%\Binaries\binkw32.dll" set "errorlevel=1"
 exit /b %errorlevel%
 
-::
-:IsElevatedCMD
-set "errorlevel=0"
-net session >nul 2>&1 || set "errorlevel=1"
-exit /b %errorlevel%
+REM ::
+REM :IsElevatedCMD
+REM set "errorlevel=0"
+REM net session >nul 2>&1 || set "errorlevel=1"
+REM exit /b %errorlevel%
 
 ::
 :HasInternetAccess
@@ -131,8 +118,15 @@ set "errorlevel=0"
 ping bitbucket.org -n 1 -w 1000 >nul 2>&1 || set "errorlevel=1"
 exit /b %errorlevel%
 
+::address, filePath
+:DownloadFile
+echo $client = New-Object System.Net.WebClient> "%temp%\%ProductName%_helper.ps1"
+echo $client.DownloadFile("%~1", "%~2")>> "%temp%\%ProductName%_helper.ps1"
+powershell -nologo -noprofile -executionpolicy bypass -file "%temp%\%ProductName%_helper.ps1"
+erase "%temp%\%ProductName%_helper.ps1"
+exit /b
+
 ::
 :Exit
-if exist "%cd%\%ProductName%_helper.ps1" erase "%cd%\%ProductName%_helper.ps1"
 timeout 2 >nul
-exit /b %errorlevel%
+exit
